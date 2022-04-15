f2cc9254d4fd8609071372f0c16a2adafa6069d3
Sazidul Rahman <100462697+sazidulcse@users.noreply.github.com>
2022-04-16 02:07:40 +0600

    on click change 16 tarik

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -8,13 +8,26 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-Button btn;
+Button sazidBtn; TextView sazidDis;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.layout3);
+        sazidBtn = findViewById(R.id.sazidBtn);
+        sazidDis = findViewById(R.id.sazidDis);
+        sazidBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sazidDis.setText("Hi This is Properly Working");
 
-
+            }
+        });
+sazidDis.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        sazidBtn.setText("buttan text change");
+    }
+});
 
 
     }
